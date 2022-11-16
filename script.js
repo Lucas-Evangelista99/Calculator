0d45321f3cd94e090b60a4b8e5dfb721bfaf518c
Lucas-Evangelista99 <Lucas_Evangelista99@hotmail.com>
2022-11-16 03:03:58 -0300

    Implement numbers and decimal functions

--- script.js
+++ script.js
@@ -25,3 +25,38 @@ function operate(operator, a, b) {
         return divide(a, b);
     }
 }
+
+let div1 = document.getElementsByClassName("typed");
+let div2 = document.getElementsByClassName("expression")
+let typed_nums = div1[0]
+let expression = div2[0]
+let numbers = document.getElementsByClassName("number")
+let decimal = document.getElementsByClassName("decimal")
+let decimal_button = decimal[0]
+
+function type_num(num) {
+    if (typed_nums.textContent == "0") {
+        typed_nums.textContent = num.textContent
+    } else {
+        typed_nums.textContent += num.textContent
+    }
+}
+
+function type_decimal(elem) {
+    if (typed_nums.textContent.includes(".")) {
+        elem.disabled = true
+    } else {
+        elem.disabled = false
+        typed_nums.textContent += elem.textContent
+    }
+}
+
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i].addEventListener("click", function(){
+        type_num(numbers[i])
+    })
+}
+
+decimal_button.addEventListener("click", function(){
+    type_decimal(decimal_button)
+})
